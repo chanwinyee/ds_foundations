1f28eaf724a4cd0a6eaf3ec7133be0cb39ecac39
chanwinyee <41238872+chanwinyee@users.noreply.github.com>
2018-10-01 21:31:48 -0700

    did two more questions for unit homework.

--- unit_homework/unit_1_q2.py
+++ unit_homework/unit_1_q2.py
@@ -1,9 +1,21 @@
-list = []
 
-for y in range(1,2001):
-    for x in range(1,2001):
-        if x <= y:
-            if isinstance(int(y)/int(x) ,int):
-                list.append(y)
+def isprime(n):
+    factors = []
+    for d in range(2,n):
+        if n%d == 0:
+            factors.append(d)
+    return len(factors) == 0
 
-print (list)
+primes = []
+
+for n in range (2,2001):
+    if isprime(n):
+        primes.append(n)
+
+print(sum(primes))
+
+
+
+# % will give me remainders; if there are remainders, then it's not divisible
+# a prime number has only two factors: 1 and itself
+# Create a list of factors for each number. If the length is 2, then it is prime

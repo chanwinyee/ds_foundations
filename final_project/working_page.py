ea09070cabf6bcbf6939b558f3acfeecb0e011e5
chanwinyee <41238872+chanwinyee@users.noreply.github.com>
2018-10-20 22:27:07 -0700

    compile data into a csv

--- final_project/working_page.py
+++ final_project/working_page.py
@@ -1,16 +1,22 @@
-# !pip install requests
-# !pip3 install requests
 import requests
 import json
 import urllib
-# import pandas as pd
+import progressbar
+
+import pandas as pd
 # import numpy as np
 # import seaborn as sns
 # import matplotlib.pyplot as plt
-# %matplotlib inline
+#%matplotlib inline
+
+from time import sleep
+bar = progressbar.ProgressBar(maxval=100, \
+    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 
 data = []
-page_number = 1
+page_number = 613
+signal = []
+
 #should_request == True
 #for i in range(3):
 
@@ -30,32 +36,21 @@ def get_bike_data():
     result.raise_for_status()
 
     if result.status_code == 200:
+        signal.clear()
+        signal.extend(result.json()['bikes'])
         #ADD TO MY DATA
         data.extend(result.json()['bikes'])
 
-while len(result['bikes']) > 0:
+get_bike_data()
+
+bar.start()
+while len(signal) > 0:
     page_number = page_number + 1
-    if len(result['bikes']) == 0:
+    get_bike_data()
+    if len(signal) == 0:
         break
 
+bike_data = pd.DataFrame(data)
+bike_data.to_csv(path_or_buf='/Users/lizchan/ds_foundations/final_project/test.csv')
 
-# dat = []
-# for i in range(3):
-#
-#     url = 'https://bikeindex.org:443/api/v3/search?'
-#     parameters = {'page:':str(i+1),
-#               'per_page':100,
-#               'location':'IP',
-#               'distance':25,
-#               'stolenness':'stolen'}
-#     qry = urllib.parse.urlencode(parameters).replace('%3A', '')
-#     s = requests.Session() #Create a Session object -  <requests.sessions.Session object at 0x1016c0198>
-#     req = requests.Request(method='GET', url=url) #<Request [GET]>
-#     prep = req.prepare() #<PreparedRequest [GET]>
-#     prep.url = url + qry #https://bikeindex.org:443/api/v3/search?page=2&per_page=100&location=IP&distance=25&stolenness=stolen
-#     result = s.send(prep) #<Response [200]>
-#     result.raise_for_status()
-#
-#     if result.status_code == 200:
-#         #ADD TO MY DATA
-#         dat.extend(result.json()['bikes'])
+bar.finish()
